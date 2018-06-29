31a67b5fd6e864eaa6d86a9b48d7820dfa3ade12
MokkeMeguru <meguru.mokke@gmail.com>
2018-06-29 19:34:05 +0900

    hoge

--- src/clj/clj_vchats/routes/services/audio.clj
+++ src/clj/clj_vchats/routes/services/audio.clj
@@ -1,25 +1,72 @@
 (ns clj-vchats.routes.services.audio
   (:require [org.httpkit.client :as http]
-            [clojure.xml :as xml]))
+            [clojure.xml :as xml]
+            ;;[clojure.data.codec :as codec]
+            )
+  (:import [javax.net.ssl SSLEngine SNIHostName SSLParameters])
+  (:use [clojure.java.shell :only [sh]]))
 
-(def api-key "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+;; --------------------------------------------------------
+(def api-key "3936337a2f48334e7a4c786c42376e746a52466f32424157352e536968474479642f6e574e465730484d33")
 
-(defn body-message [text voice]
+(defn sni-configure [#^SSLEngine ssl-engine uri]
+  (let [#^SSLParameters ssl-params (.getSSLParameters ssl-engine)]
+    (.setServerNames ssl-params [(SNIHostName. (.getHost uri))])
+    (.setSSLParameters ssl-engine ssl-params)))
+
+(def client (http/make-client {:ssl-configurer sni-configure}))
+
+;; -------------------------------
+
+(defn body-message [text voice rate pitch]
   (with-out-str
     (xml/emit
      {:tag :speak :attrs {:version "1.1"}
-      :content [
-                {:tag :voice :attrs {:name voice}}
-                text]})))
+      :content [{:tag :voice :attrs {:name voice}
+                 :content [{:tag :prosody :attrs
+                            {:rate rate
+                             :pitch pitch}
+                            :content [text]}]}]})))
 
 (defn get-audio [#^String room #^String text
-                 & {:keys [voice] :or {voice "sumire"}}]
+                 & {:keys [voice rate pitch] :or {voice "sumire" rate 1.0 pitch 1.0}}]
   (let [dt (clj-time.coerce/to-long (clj-time.local/local-now))
         {:keys [status headers body error] :as resp}
-        @(http/post
-          (str "https://api.apigw.smt.docomo.ne.jp/aiTalk/v1/textToSpeech?APIKEY="
-               api-key)
-          :body (body-message text message))
-        ;; (with-open [w (clojure.java.io/writer (str "./" room "/" dt ".mp3")) :append false])
-        ]))
+        @(http/request
+         {:url (str "https://api.apigw.smt.docomo.ne.jp/aiTalk/v1/textToSpeech?APIKEY="
+                    api-key)
+          :method :post
+          :headers {"Content-Type" "application/ssml+xml"
+                    "Accept" "audio/L16"}
+          :keepalive 3000
+          :client client
+          :body (body-message text voice rate pitch)
+          })]
+    (if-not error body nil)))
+
+;; (body-message "こんにちわ" "sumire")
+
+(defn get->tmp->bin [bin]
+  (when bin
+    (let [tfile (java.io.File/createTempFile "./tmp-" ".raw")
+          t-path (.getAbsolutePath tfile)
+          _ (with-open
+              [w (clojure.java.io/output-stream tfile)]
+              (.write w bin))
+          ofile (java.io.File/createTempFile "./out-" ".wav")
+          o-path (.getAbsolutePath ofile)
+          _ (sh "sox" "-t" "raw" "-r" "16k" "-e"
+                "signed" "-b" "16" "-B" "-c" "1" t-path o-path)
+          _ (.delete tfile)
+          copy-buf (with-open [in (clojure.java.io/input-stream ofile)]
+                     (let [buf (byte-array (.length ofile))
+                           _ (.read in buf)]
+                       buf))]
+      copy-buf)))
+
+
+;; (let [buf (get->tmp->bin (.bytes (get-audio "elect" "こんにちは" :voice "sumire" :rate 1.25 :pitch 2.0)))]
+;;   (with-open [w (clojure.java.io/output-stream (java.io.File/createTempFile "./test-" ".wav"))]
+;;     (.write w buf)))
+
 
